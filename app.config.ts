5ba0d567dae643a50e9b0b72aafc0e3daf83795f
wobsoriano <sorianorobertc@gmail.com>
2024-12-11 20:55:23 -0800

    test fix

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@tanstack/start/config";
-import tsConfigPaths from 'vite-tsconfig-paths'
 
 import { cloudflare } from "unenv";
 
@@ -7,13 +6,5 @@ export default defineConfig({
   server: {
     preset: "cloudflare-pages",
     unenv: cloudflare,
-    // rollupConfig: {
-    //   external: ["node:async_hooks"],
-    // },
-  },
-  vite: {
-    plugins: [tsConfigPaths({
-            projects: ['./tsconfig.json'],
-          })]
   }
 });
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,10 @@ import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/tanstack-start";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
+  beforeLoad: ({ context }) => {
+    // console.log(process.env.MY_KEY);
+    // console.log(import.meta.env.MY_KEY);
+  },
   component: Home,
 });
 
